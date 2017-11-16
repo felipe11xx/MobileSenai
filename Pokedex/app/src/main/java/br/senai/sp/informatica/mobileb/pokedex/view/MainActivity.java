4c3c5d58e008a6a81eb5c0f2f90adda0ee5b34a7
WEB <felipe11xx@hotmail.com>
2017-11-16 21:56:21 -0200

    continuação da Pokedex

--- Pokedex/app/src/main/java/br/senai/sp/informatica/mobileb/pokedex/view/MainActivity.java
+++ Pokedex/app/src/main/java/br/senai/sp/informatica/mobileb/pokedex/view/MainActivity.java
@@ -3,6 +3,8 @@ package br.senai.sp.informatica.mobileb.pokedex.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
@@ -32,4 +34,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int idIconMenu = item.getItemId();
+
+        switch (idIconMenu){
+            case R.id.addIcon:
+
+                break;
+            case R.id.exitIcon:
+
+                this.finishAffinity();
+                break;
+        }
+
+        return true;
+    }
 }
