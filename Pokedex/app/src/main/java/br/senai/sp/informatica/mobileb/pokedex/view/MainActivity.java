c757b91a40b24fa0269fe81f6098a61d5c17f12d
WEB <felipe11xx@hotmail.com>
2017-12-07 21:40:51 -0200

    ok navchatisse

--- Pokedex/app/src/main/java/br/senai/sp/informatica/mobileb/pokedex/view/MainActivity.java
+++ Pokedex/app/src/main/java/br/senai/sp/informatica/mobileb/pokedex/view/MainActivity.java
@@ -2,15 +2,21 @@ package br.senai.sp.informatica.mobileb.pokedex.view;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,11 +32,17 @@ public class MainActivity extends AppCompatActivity {
     private BaseAdapter itemLista;
     private Intent i;
     private final int EDITAR_POKE = 0;
+    private static int NEW_ACTION = 1;
+    private static int PREF_ACTION = 2;
     private MenuItem lixoItem,voltaItem,addItem;
     private List<Long> removeId;
     private PokemonDao dao = PokemonDao.manager;
     private Pokemon poke;
-
+    private DrawerLayout drawer;
+    // Atributos utilizados no Navigation Drawer
+    private TextView tvNome;
+    private TextView tvEmail;
+    private ImageView ivFoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +52,34 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.lvLista);
         listView.setAdapter(itemLista);
         removeId =  new ArrayList<>();
+
+
+        // Configuração do ToolBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configuração no Menu de Navegação
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Registro dos menu para tratar as ações do menu de navegação
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+       // navigationView.setNavigationItemSelectedListener(this);
+
+        // Inicializa oa atributor do Navigation Drawer
+        View cabecalho = navigationView.getHeaderView(0);
+        tvNome = (TextView)cabecalho.findViewById(R.id.txtNomePessoa);
+        tvEmail = (TextView)cabecalho.findViewById(R.id.txtEmail);
+        ivFoto = (ImageView)cabecalho.findViewById(R.id.imgPessoa);
+
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
 
 
                 poke = dao.getPokemon(id);
